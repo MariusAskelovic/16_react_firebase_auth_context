fb7480fcbdfa6d636951c469fd96693b8c565384
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-28 11:23:46 +0300

    onAuthStateChange

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,6 +1,8 @@
 import { Link, NavLink } from 'react-router-dom';
 import './Header.scss';
 
+const isLoggedIn = false;
+
 export default function Header() {
   return (
     <header className='header'>
@@ -12,12 +14,21 @@ export default function Header() {
           <NavLink className={'navLink'} to={'/'}>
             Home
           </NavLink>
-          <NavLink className={'navLink'} to={'/profile'}>
-            My Profile
-          </NavLink>
-          <NavLink className={'navLink'} to={'/login'}>
-            Login
-          </NavLink>
+          {isLoggedIn && (
+            <NavLink className={'navLink'} to={'/profile'}>
+              My Profile
+            </NavLink>
+          )}
+          {!isLoggedIn && (
+            <NavLink className={'navLink'} to={'/login'}>
+              Login
+            </NavLink>
+          )}
+          {isLoggedIn && (
+            <NavLink className={'navLink'} to={'/login'}>
+              Logout
+            </NavLink>
+          )}
         </nav>
       </div>
     </header>
