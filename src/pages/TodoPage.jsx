03249c5825c3012221cf38ce0cb1904fb337d211
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-31 11:36:03 +0300

    delete veikia

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,8 +1,15 @@
-import { collection, getDocs } from 'firebase/firestore';
+import {
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+} from 'firebase/firestore';
 import { db } from '../firebase/firebase';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 export default function TodoPage() {
   const [booksArr, setBooksArr] = useState([]);
+
   async function getBooksFb() {
     const querySnapshot = await getDocs(collection(db, 'books'));
     const dataBack = [];
@@ -24,17 +31,52 @@ export default function TodoPage() {
     // console.log('dataBack ===', dataBack);
     setBooksArr(dataBack);
   }
+  getBooksFb();
+  useEffect(() => {
+    getBooksFb();
+  }, []);
+
+  const newBook = {
+    title: 'The Great Gatsby',
+    year: 1925,
+    author: 'F. Scott Fitzgerald',
+    isOnSale: true,
+  };
+
+  async function createBook() {
+    try {
+      const docRef = await addDoc(collection(db, 'books'), newBook);
+      console.log('Document written with ID: ', docRef.id);
+    } catch (e) {
+      console.error('Error adding document: ', e);
+    }
+  }
+  async function handleDelete(idToDelete) {
+    console.log('idToDelete ===', idToDelete);
+    try {
+      const rez = await deleteDoc(doc(db, 'books', idToDelete));
+      console.log('pavyko delete ===', rez);
+      getBooksFb();
+    } catch (error) {
+      console.warn('ivyko klaida:', error);
+    }
+  }
   return (
     <div className='contaienr'>
       <h1>Todo page</h1>
       <p>make your todos</p>
       <div>
-        <button onClick={getBooksFb}>get books data</button>
+        <button onClick={() => {}}>get books data</button>
+        <button onClick={createBook}>create book</button>
       </div>
 
       <ul className='unlisted'>
         {booksArr.map((bookObj) => (
-          <li key={bookObj.id}>TITLE: {bookObj.title}</li>
+          <li key={bookObj.id}>
+            TITLE: {bookObj.title}
+            {'  '}
+            <button onClick={() => handleDelete(bookObj.id)}>El Deleto</button>
+          </li>
         ))}
       </ul>
     </div>
